c086c4b804485a5531eb5c310fdec62477c413c0
saharazimi619 <sahar.azimii.619@gmail.com>
2025-01-03 13:33:02 +0330

    file modified

--- beginner_projects/py/empty_character.py
+++ beginner_projects/py/empty_character.py
@@ -1,24 +1,12 @@
-# def myfun(text,ch):
-#     result=''
-#     for i in text:
-#         if i==' ':
-#             i=ch
-#         result+=i
-#     return result
+def empty_character(text,ch):
+    result=''
+    for i in text:
+        if i==' ':
+            i=ch
+        result+=i
+    return result
 
-# text='D t C mpBl ckFrid yS le'
-# ch='a'
-# print(myfun(text,ch))        
-
-
-
-string1='Hello!MyN meIsS h r.Iw ntToLe rnPython!'
-result=''
+text='D t C mpBl ckFrid yS le'
 ch='a'
-for i in string1:
-    if i==' ':
-        i=ch
-    result+=i 
-
-print(result)       
+print(empty_character(text,ch))        
 
