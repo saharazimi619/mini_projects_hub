9ae554cb8a6e1be9aeeea2b4331d0c496039772f
saharazimi619 <sahar.azimii.619@gmail.com>
2025-01-03 12:15:47 +0330

    file added

---
+++ beginner_projects/py/empty_character.py
@@ -0,0 +1,24 @@
+# def myfun(text,ch):
+#     result=''
+#     for i in text:
+#         if i==' ':
+#             i=ch
+#         result+=i
+#     return result
+
+# text='D t C mpBl ckFrid yS le'
+# ch='a'
+# print(myfun(text,ch))        
+
+
+
+string1='Hello!MyN meIsS h r.Iw ntToLe rnPython!'
+result=''
+ch='a'
+for i in string1:
+    if i==' ':
+        i=ch
+    result+=i 
+
+print(result)       
+
